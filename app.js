b91e9b4128e7b85c5ea4aa1c66a9c8d70c0b7381
dimitrijevich <dimitrijevicraf@gmail.com>
2024-09-24 15:42:00 +0200

    updated args

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
@@ -12,55 +13,64 @@ async function scrape(url, username) {
   let browser;
   try {
     browser = await puppeteer.launch({
-      headless: true,
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",        
+        ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
 
-    await page.type('form > input', username);
-    await page.keyboard.press('Enter')
+    await page.type("form > input", username);
+    await page.keyboard.press("Enter");
 
     const values = {
-        num_of_posts: 'div > ul > li:nth-child(1) > span:nth-child(1)',
-        followed: 'div > ul > li:nth-child(2) > span:nth-child(1)',
-        following: 'div > ul > li:nth-child(3) > span:nth-child(1)',
+      num_of_posts: "div > ul > li:nth-child(1) > span:nth-child(1)",
+      followed: "div > ul > li:nth-child(2) > span:nth-child(1)",
+      following: "div > ul > li:nth-child(3) > span:nth-child(1)",
+    };
+
+    const img_selector = ".avatar__image";
+    const name_selector = ".user-info__full-name";
+    const bio_selector = ".user-info__biography";
+
+    const results = {};
+
+    for (key in values) {
+      const selector = values[key];
+      await page.waitForSelector(selector, { timeout: 30000 });
+      const element = await page.$(selector);
+      if (element) {
+        const textProperty = await element.getProperty("textContent");
+        results[key] = await textProperty.jsonValue();
+      }
     }
 
-    const img_selector = '.avatar__image'
-    const name_selector = '.user-info__full-name'
-    const bio_selector = '.user-info__biography'
-
-    const results = {}
-    
-    for(key in values){
-        const selector = values[key]
-        await page.waitForSelector(selector, { timeout: 30000 });
-        const element = await page.$(selector)
-        if(element){
-            const textProperty = await element.getProperty("textContent");
-            results[key] = await textProperty.jsonValue()
-        }
+    const img_element = await page.$(img_selector);
+    if (img_element) {
+      const srcProperty = await img_element.getProperty("src");
+      const img = await srcProperty.jsonValue();
+      results["image"] = img;
     }
 
-    const img_element = await page.$(img_selector)
-    if(img_element){
-        const srcProperty = await img_element.getProperty('src')
-        const img = await srcProperty.jsonValue()
-        results['image'] = img
+    const name_element = await page.$(name_selector);
+    if (name_element) {
+      const textProperty = await name_element.getProperty("textContent");
+      const name = await textProperty.jsonValue();
+      results["full_name"] = name;
     }
 
-    const name_element = await page.$(name_selector)
-    if(name_element){
-        const textProperty = await name_element.getProperty('textContent')
-        const name = await textProperty.jsonValue()
-        results['full_name'] = name
-    }
-
-    const bio_element = await page.$(bio_selector)
-    if(bio_element){
-        const textProperty = await bio_element.getProperty('textContent')
-        const bio = await textProperty.jsonValue()
-        results['bio'] = bio
+    const bio_element = await page.$(bio_selector);
+    if (bio_element) {
+      const textProperty = await bio_element.getProperty("textContent");
+      const bio = await textProperty.jsonValue();
+      results["bio"] = bio;
     }
 
     return results;
