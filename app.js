033b5d5d326030be9115566e9fbd7c184eeded48
ilijja <dimitrijevich@ymail.com>
2025-04-17 01:50:02 +0200

    new

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const LimitingMiddleware = require("limiting-middleware");
 const cors = require("cors");
 const axios = require("axios");
+const { timeout } = require("puppeteer");
 
 const app = express();
 app.use(express.json());
@@ -32,7 +33,7 @@ function delay(time) {
   });
 }
 
-async function scrape(url, username) {
+async function scrape(url, uname) {
   let browser;
   let page;
 
@@ -46,6 +47,7 @@ async function scrape(url, username) {
             "--single-process",
             "--no-zygote",
           ],
+          // headless: false,
           executablePath:
             process.env.NODE_ENV === "production"
               ? process.env.PUPPETEER_EXECUTABLE_PATH
@@ -53,48 +55,121 @@ async function scrape(url, username) {
         });
 
         page = await browser.newPage();
-        await page.goto(url, { waitUntil: "networkidle0" });
 
-        await page.waitForSelector(".search-form__input", { timeout: 3000 });
-        await page.type(".search-form__input", username);
-        await page.keyboard.press("Enter");
+        //ODAVDE JE NOVI KOD
+        const final = `${url}/${uname}/`;
+        await page.goto(final, { waitUntil: "domcontentloaded" });
 
-        await page.waitForSelector(".user-info__username-text", {
-          timeout: 3000,
+        await Promise.all([
+          page.waitForSelector("h1.profile__nickname", { timeout: 20000 }),
+          page.waitForSelector("div.profile__description", { timeout: 20000 }),
+          page.waitForSelector("ul.profile__stats li.profile__stats-item", {
+            timeout: 20000,
+          }),
+        ]).catch(() => {
+          console.log("Neki elementi se nisu pojavili u predviđenom vremenu");
         });
 
-        const posts = await page.$eval(
-          ".stats__item:nth-child(1) > span:nth-child(1)",
-          (el) => el.innerText
-        );
-        const followers = await page.$eval(
-          ".stats__item:nth-child(2) > span:nth-child(1)",
-          (el) => el.innerText
-        );
-        const following = await page.$eval(
-          ".stats__item:nth-child(3) > span:nth-child(1)",
-          (el) => el.innerText
+        let stats;
+        let retryCount = 0;
+        const maxRetries = 10;
+        const retryDelay = 3000; 
+
+        do {
+          stats = await page.$$eval(
+            "ul.profile__stats li.profile__stats-item span",
+            (elements) =>
+              elements.map((el) => {
+                const text = el.textContent.trim();
+                const numbers = text.match(/\d+/g);
+                return numbers ? numbers.join("") : "";
+              })
+          );
+
+          if (stats.every(stat => stat === "0")) {
+            console.log(`Svi statovi su 0. Pokušaj ${retryCount + 1}/${maxRetries}`);
+            await new Promise(resolve => setTimeout(resolve, retryDelay));
+            retryCount++;
+          } else {
+            break; 
+          }
+        } while (retryCount < maxRetries);
+
+        const username = await page.$eval("h1.profile__nickname", (el) => {
+          const rawText = el.textContent.trim();
+          const words = rawText.split(/\s+/);
+          return words[0];
+        });
+
+        const bio = await page.$eval("div.profile__description", (el) =>
+          el.textContent.trim()
         );
 
-        const image = await page
-          .$eval(".avatar__image", (el) => el.src)
-          .catch(() => "");
-        const fullName = await page
-          .$eval(".user-info__full-name", (el) => el.innerText)
-          .catch(() => "");
-        const bio = await page
-          .$eval(".user-info__biography", (el) => el.innerText)
-          .catch(() => "");
+        page.waitForSelector("img.profile__avatar-pic", { timeout: 20000 });
 
-        return {
-          username,
-          posts: posts || 0,
-          full_name: fullName,
-          followers: followers || 0,
-          following: following || 0,
-          bio,
-          image,
-        };
+        await page.waitForFunction(
+          () => {
+            const img = document.querySelector("img.profile__avatar-pic");
+            return (
+              img && img.src && !img.src.startsWith("data:") && img.complete
+            );
+          },
+          { timeout: 20000 }
+        );
+
+        const image = await page.$eval(
+          "img.profile__avatar-pic",
+          (el) => el.src
+        );
+
+        const posts = stats[0];
+        const followers = stats[1];
+        const following = stats[2];
+
+        return { username, bio, posts, followers, following, image };
+
+        /* DOVDE JE NOVI KOD */
+
+        // await page.waitForSelector(".search-form__input", { timeout: 3000 });
+        // await page.type(".search-form__input", username);
+        // await page.keyboard.press("Enter");
+
+        // await page.waitForSelector(".user-info__username-text", {
+        //   timeout: 3000,
+        // });
+
+        // const posts = await page.$eval(
+        //   ".stats__item:nth-child(1) > span:nth-child(1)",
+        //   (el) => el.innerText
+        // );
+        // const followers = await page.$eval(
+        //   ".stats__item:nth-child(2) > span:nth-child(1)",
+        //   (el) => el.innerText
+        // );
+        // const following = await page.$eval(
+        //   ".stats__item:nth-child(3) > span:nth-child(1)",
+        //   (el) => el.innerText
+        // );
+
+        // const image = await page
+        //   .$eval(".avatar__image", (el) => el.src)
+        //   .catch(() => "");
+        // const fullName = await page
+        //   .$eval(".user-info__full-name", (el) => el.innerText)
+        //   .catch(() => "");
+        // const bio = await page
+        //   .$eval(".user-info__biography", (el) => el.innerText)
+        //   .catch(() => "");
+
+        // return {
+        //   username,
+        //   posts: posts || 0,
+        //   full_name: fullName,
+        //   followers: followers || 0,
+        //   following: following || 0,
+        //   bio,
+        //   image,
+        // };
       } catch (error) {
         console.error(`Attempt ${attempt + 1} failed:`, error);
         if (page) await page.close();
@@ -113,28 +188,26 @@ async function scrape(url, username) {
 }
 
 app.post("/scrape", async (req, res) => {
-  
   try {
     if (!checkOrigin(req.headers.origin)) {
       return res.status(404).json({ msg: "not allowed" });
     }
 
-    
     const username = req.body.username;
 
     if (!username) {
       return res.status(400).json({ error: "Username is required" });
     }
 
-    const url = `https://fastdl.app/instagram-anonymously-viewer`;
+    const url = `https://insta-stories-viewer.com`;
     const result = await scrape(url, username);
-    
-    console.log(result);
-    
+
     // const url = `https://fanhub.pro/instablogs_ig_viewer?username=${username}`;
 
     // const result = await axios.get(url);
-    res.json(result)
+    console.log(result);
+
+    res.json(result);
 
     // res.json({
     //   username: result.username,
